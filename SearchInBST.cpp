fa21a1a57ce295cfa9b874e82d76da616b645ae9
Mehul Thuletiya <210303108237@paruluniversity.ac.in>
2023-07-09 16:34:32 +0530

    137 - Search in BST
    Easy

---
+++ SearchInBST.cpp
@@ -0,0 +1,15 @@
+
+#include <bits/stdc++.h> 
+
+bool searchInBST(BinaryTreeNode<int> *root, int x) {
+
+    if(root==NULL)
+        return false ;
+    if (x==root->data)
+        return true;
+    if(x<root->data)
+        return searchInBST(root->left, x);
+    else
+        return searchInBST(root->right, x);
+
+}
