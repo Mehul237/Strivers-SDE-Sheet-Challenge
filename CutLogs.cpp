bfb5596b5740659362d39c8016a338cfd708b2cb
Mehul Thuletiya <210303108237@paruluniversity.ac.in>
2023-07-21 22:09:54 +0530

    182 - Cut Logs

--- CutLogs.cpp
+++ CutLogs.cpp
@@ -1,13 +1,32 @@
+#include<bits/stdc++.h>
 
-int f(int k, int start, int end) {
-    if(end == start)
-        return 0;
-    if(k == 1)
-        return (end-start);
-    int mid = (start + end + 1)/2;
-    return 1 + max(f(k-1, start, mid-1), f(k, mid, end));
-}
+int dp[104][10004];
 
 int cutLogs(int k, int n) {
-    return f(k, 0, n);
+    vector<int> prev(n + 1, 0), curr(n + 1, 0);
+    prev[1] = 1;
+
+    for(int f = 1; f <= n; f++)
+        prev[f] = curr[f] = f;
+
+    for(int e = 2; e <= k; e++) {
+        for(int f = 2; f <= n; f++) {
+            int low = 1, high = f, ans = INT_MAX;
+            while(low <= high) {
+                int mid = (low + high) >> 1;
+                int down = prev[mid - 1];
+                int up = curr[f - mid];
+
+                ans = min(ans, 1 + max(down, up));
+                if(up > down)
+                    low = mid + 1;
+                else 
+                    high = mid - 1;
+            }
+
+            curr[f] = ans;
+        }
+        prev = curr;
+    }   
+    return prev[n];
 }
